8c56f502993b8f66408b7d3c08674cca88082b7d
ObviouslyLuuk <54840183+ObviouslyLuuk@users.noreply.github.com>
2024-10-30 22:12:48 +0100

    improved preprocess_latents.py; added to README; added images; added results.zip;
    made preprocess_latents.py general for any data dir and removed the notebook version; added summary image and folder structure to README; added images; added results.zip for visualizing results and generating figures;

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 
 This project was done in collaboration with the Pathology department at Amsterdam UMC, location AMC, under supervision of Prof. Erik J. Bekkers, Michel Botros and Dr. Sybren Meijer. The goal was to use different AI model architectures for the task of detecting Barret's Esophagus (a precursor for Esophageal Cancer) lesions in microscopic scans of biopsies from the esophagus, stained with p53 immunohistochemistry.
 
-The main model foundations used were CLAM and ResNet, with RetCCL used for feature extraction. I added double-binary classification, and added synthetic data to the dataset to improve the model's performance. The models were trained/tested on part of the LANS dataset and on the BOLERO dataset.
+The main model foundations used were CLAM and ResNet, with pretrained RetCCL used for feature extraction. I adapted the model architectures for double-binary classification, and added synthetic data to the dataset to improve the model's performance. The models were trained/tested on part of the LANS dataset and on the BOLERO dataset, which were provided to me by the Pathology department at AUMC.
+
+<!-- Research summary image from images/summary.png: -->
+![Research Summary](images/summary.png)
+In the above figure you can see an overview of the used full-biopsy and CLAM architectures in (a), the double-binary classification in (b), and CLAM attention maps in (c).
 
 ## Main files
 - `vis_data.ipynb`: Visualize some aspects of the data like distribution
@@ -22,9 +26,13 @@ The main model foundations used were CLAM and ResNet, with RetCCL used for featu
 - `eval.py`: Contains the evaluation functions to use during PyTorch Lightning training
 
 ## Running the code
-`requirements.txt` contains the necessary packages to run the code. However, I just used pip freeze -> requirements.txt, so it might contain more than necessary (sorry about that).
+`requirements.txt` contains the necessary packages to run the code. However, I just used pip freeze -> requirements.txt, so it might contain more than necessary (sorry about that). Python 3.11.5 was used for this project.
 
-Before running, the code expects a data root directory "../../data" to be present, but this path can be changed at the top of `load_data.py`. In that root should be directories for each dataset (for example "LANS" and "BOLERO"). The main dataset (LANS in my case) should have the following structure:
+### Only running to visualize results
+In `minimum_data/results.zip` you can find the results of the trained models on the test set. Without any additional data, you can run the `vis_results.ipynb` notebook once you have the `results/` folder from the zip file set up at the path specified in the first notebook cell. This will load the predictions and generate the statistics and figures inside the notebook.
+
+### Training the models
+Before training, the code expects a data root directory `../../data` to be present, but this path can be changed at the top of `load_data.py`. In that root should be directories for each dataset (for example "LANS" and "BOLERO"). The main dataset (LANS in my case) should have the following structure:
 - `biopsies/` containing the biopsy pngs, with names like "11_3.png" for the 11th slide and 3rd biopsy on that slide
 - `train.csv` and `test.csv` containing the image names and labels like:
 ```
@@ -35,12 +43,55 @@ id,label
 ```
 where 0 is Wild Type, 1 is Overexpression, 2 is Null Mutation and 3 is Double Clones.
 
-For BOLERO, the biopsies/ directory is also expected, but aside from that a labels file "P53_BOLERO_T.csv" with all pathologist ratings is expected in the form of:
+For BOLERO, the `biopsies/` directory is also expected, but aside from that a labels file `P53_BOLERO_T.csv` with all pathologist ratings is expected in the form of:
 ```
 Case ID,{pathologist id}, ... ,GS
 {case id},{pathologist rating}, ... ,{gold standard label}
 ...
 ```
+The CLAM models expect files with preprocessed latent vectors for each image (their path can be specified in the arguments of the `pl_clam.py` script) which can be generated using the `preprocess_latents.py` script.
 
-### Only running for the results
-If you have the results/ directory with the saved model predictions, you can run the `vis_results.ipynb` file without any other data. This will load the predictions and generate the statistics and figures.
+### Folder structure
+The folder structure can of course be changed by adjusting the paths in the code, but the following structure is expected by default:
+```
+code
+├── {this repository, should be cwd when running scripts}
+│   ├── {all files in this repository}
+│
+results (only necessary if visualizing existing results, can be found in minimum_data/results.zip)
+├── CLAM
+│   ├── {results for CLAM models .pt files}
+│
+├── {other model directories}
+│
+data (only necessary if training or evaluating models)
+├── biopsies_s1.0_anon_new
+│   ├── biopsies
+│   │   ├── 0_0.png
+│   │   ├── 0_1.png
+│   │   ├── ...
+│   │
+│   ├── masks (optional)
+│   │   ├── 0_0.png
+│   │   ├── 0_1.png
+│   │   ├── ...
+│   │
+│   ├── train.csv
+│   ├── test.csv
+│   ├── (latent vector files for CLAM models will be saved here)
+│
+├── BOLERO
+│   ├── biopsies
+│   │   ├── 0_0.png
+│   │   ├── 0_1.png
+│   │   ├── ...
+│   │   
+│   ├── P53_BOLERO_T.csv
+│   ├── (latent vector files for CLAM models will be saved here)
+│
+models (only necessary if evaluating existing models)
+├── CLAM
+│   ├── {trained CLAM checkpoints}
+│
+├── {other model directories}
+```
--- preprocess_latents.py
+++ preprocess_latents.py
@@ -1,12 +1,11 @@
 import torch
 import torchvision
 import torch.nn as nn
-import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
 
-DATA_DIR = "../../data/biopsies_s1.0_anon_new/"
+from load_data import DATA_DIR
 BOLERO_DIR = os.path.join(DATA_DIR, "..", "BOLERO")
 
 # Device configuration
@@ -14,15 +13,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # device = torch.device('cpu')
 print("Device: {}".format(device))
 
-# Load csv
-csv_file = os.path.join(DATA_DIR, "biopsy_labels_s1.0_anon.csv")
-main_df = pd.read_csv(csv_file)
 
-bolero_files = os.listdir(os.path.join(BOLERO_DIR, "biopsies"))
-bolero_indices = [f.split(".")[0].split('_') for f in bolero_files]
-bolero_df = pd.DataFrame(bolero_indices, columns=["case", "biopsy"])
-
-
+# Load RetCCL model (feature extractor)
 from RetCCL.custom_objects import retccl_resnet50, HistoRetCCLResnet50_Weights
 
 retccl = retccl_resnet50(weights=HistoRetCCLResnet50_Weights.RetCCLWeights).to(device)
@@ -49,7 +41,6 @@ model_configs = {
     "retccl": {
         "model": retccl,
         "size": 256,
-        # "size": None,
         "num_ftrs": 2048,
         "transform": TRANSFORM,
         "spacing": 1.0,
@@ -57,76 +48,82 @@ model_configs = {
 }
 
 
-model_name = "retccl"
-model_config = model_configs[model_name]
-model = model_config["model"]
-model.eval()
-model = model.to(device)
+def process_bag_latents(model_name="retccl", root_dir=DATA_DIR):
+    print(f"Processing bag latents with {model_name} in {root_dir}...")
+    model_config = model_configs[model_name]
+    model = model_config["model"]
+    model.eval()
+    model = model.to(device)
 
-size = model_config["size"]
-resize_factor = 1 / model_config["spacing"]
-transform = model_config["transform"]
-quantile_range_threshold = 0.1
-root_dir = DATA_DIR
+    size = model_config["size"]
+    resize_factor = 1 / model_config["spacing"]
+    transform = model_config["transform"]
+    num_ftrs = model_config["num_ftrs"]
+    quantile_range_threshold = 0.1
 
-df = main_df
+    biopsy_ids = [f.split(".")[0] for f in os.listdir(os.path.join(root_dir, "biopsies"))]
 
-num_ftrs = model_config["num_ftrs"]
+    indices_filename = os.path.join(root_dir, "non_empty_patch_indices_gs256.pt")
+    non_empty_patch_indices_by_biopsy = {}
+    if os.path.exists(indices_filename):
+        non_empty_patch_indices_by_biopsy = torch.load(indices_filename)
 
-indices_filename = os.path.join(root_dir, "non_empty_patch_indices_gs256.pt")
-non_empty_patch_indices_by_biopsy = {}
-if os.path.exists(indices_filename):
-    non_empty_patch_indices_by_biopsy = torch.load(indices_filename)
-
-# Load latents
-latent_file = os.path.join(root_dir, f"bag_latents_gs{size}_{model_name}.pt")
-if os.path.exists(latent_file):
-    bag_latents = torch.load(latent_file)
-else:
+    # Load latents
+    latent_file = os.path.join(root_dir, f"bag_latents_gs{size}_{model_name}.pt")
     bag_latents = {}
-for i, (idx, label) in tqdm(enumerate(df.values), total=len(df)):
-    if idx in bag_latents:
-        continue
+    if os.path.exists(latent_file):
+        bag_latents = torch.load(latent_file)
 
-    img_file = os.path.join(root_dir, "biopsies", f"{idx}.png")
-    img = plt.imread(img_file)
-    img = torch.tensor(img).permute(2, 0, 1).float() # (3, h, w)
+    for i, idx in enumerate(tqdm(biopsy_ids)):
+        if idx in bag_latents:
+            continue
+        # if idx in non_empty_patch_indices_by_biopsy:
+        #     continue
 
-    # Resize the img so that the height and width are multiples of grid_spacing, 
-    # rounded to the nearest multiple.
-    # This prevents the last row and column of patches from being cut off.
-    # We also resize the image by the resize_factor instead of each patch separately
-    new_size = (max(round(img.shape[1]*resize_factor/size), 1)*size, 
-                max(round(img.shape[2]*resize_factor/size), 1)*size)
-    img = torch.nn.functional.interpolate(img.unsqueeze(0), size=new_size, 
-        mode='bilinear', align_corners=False)[0]
+        img_file = os.path.join(root_dir, "biopsies", f"{idx}.png")
+        img = plt.imread(img_file)
+        img = torch.tensor(img).permute(2, 0, 1).float() # (3, h, w)
 
-    patches = torch.nn.functional.unfold(img.unsqueeze(0), 
-        kernel_size=(size, size), 
-        stride=(size, size)) # (1, 3*size*size, n_patches)
-    patches = patches.permute(0, 2, 1).reshape(-1, 3, size, size) # (n_patches, 3, size, size)
-    
-    # Remove patches with low difference between the 75th and 1st percentile
-    qranges = torch.quantile(patches.view(patches.shape[0], -1), 0.75, dim=-1) - \
-              torch.quantile(patches.view(patches.shape[0], -1), 0.01, dim=-1)
-    non_empty_idx = torch.where(qranges >= quantile_range_threshold)[0]
-    patches = transform(patches[non_empty_idx])
-    patches = patches.to(device)
+        # Resize the img so that the height and width are multiples of grid_spacing, 
+        # rounded to the nearest multiple.
+        # This prevents the last row and column of patches from being cut off.
+        # We also resize the image by the resize_factor instead of each patch separately
+        new_size = (max(round(img.shape[1]*resize_factor/size), 1)*size, 
+                    max(round(img.shape[2]*resize_factor/size), 1)*size)
+        img = torch.nn.functional.interpolate(img.unsqueeze(0), size=new_size, 
+            mode='bilinear', align_corners=False)[0]
 
-    non_empty_patch_indices_by_biopsy[idx] = non_empty_idx
+        patches = torch.nn.functional.unfold(img.unsqueeze(0), 
+            kernel_size=(size, size), 
+            stride=(size, size)) # (1, 3*size*size, n_patches)
+        patches = patches.permute(0, 2, 1).reshape(-1, 3, size, size) # (n_patches, 3, size, size)
+        
+        # Remove patches with low difference between the 75th and 1st percentile
+        qranges = torch.quantile(patches.view(patches.shape[0], -1), 0.75, dim=-1) - \
+                torch.quantile(patches.view(patches.shape[0], -1), 0.01, dim=-1)
+        non_empty_idx = torch.where(qranges >= quantile_range_threshold)[0]
+        patches = transform(patches[non_empty_idx])
+        patches = patches.to(device)
 
-    with torch.no_grad():
-        patch_latents = model(patches) # (n_patches, num_ftrs)
-    bag_latents[idx] = patch_latents.cpu()[:, None, :] # (n_patches, 1, num_ftrs)
+        non_empty_patch_indices_by_biopsy[idx] = non_empty_idx
 
-    if i % 10 == 0:
-        torch.save(bag_latents, latent_file)
-        torch.save(non_empty_patch_indices_by_biopsy, indices_filename)
+        with torch.no_grad():
+            patch_latents = model(patches) # (n_patches, num_ftrs)
+        bag_latents[idx] = patch_latents.cpu()[:, None, :] # (n_patches, 1, num_ftrs)
 
-    # Clear memory
-    del img, patches, patch_latents
-    torch.cuda.empty_cache()
+        if i % 10 == 0:
+            torch.save(bag_latents, latent_file)
+            torch.save(non_empty_patch_indices_by_biopsy, indices_filename)
 
-torch.save(bag_latents, latent_file)
-torch.save(non_empty_patch_indices_by_biopsy, indices_filename)
+        # Clear memory
+        del img, patches, patch_latents
+        torch.cuda.empty_cache()
+
+    torch.save(bag_latents, latent_file)
+    torch.save(non_empty_patch_indices_by_biopsy, indices_filename)
+
+
+if __name__ == "__main__":
+    process_bag_latents(model_name="retccl", root_dir=DATA_DIR)
+    process_bag_latents(model_name="retccl", root_dir=BOLERO_DIR)
 
